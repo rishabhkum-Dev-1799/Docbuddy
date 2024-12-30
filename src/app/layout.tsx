9aac0afa76adad0e52da5f20e10ec527d716df59
rishabhkum-Dev-1799 <rishabhkum17@gmail.com>
2024-12-30 19:46:15 +0530

    init-Initial set up commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Docubuddy',
+  description: 'Chat with Your Docs anywhere and Anytime',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
